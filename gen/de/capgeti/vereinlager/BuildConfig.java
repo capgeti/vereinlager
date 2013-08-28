b42d333feb48b906c7ea4b2a2f744c4ab35bd033
Michael Wolter <michael.wolter@atns.de>
2013-08-28 14:16:09 +0200

    init

---
+++ gen/de/capgeti/vereinlager/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package de.capgeti.vereinlager;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
