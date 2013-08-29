1dae6d1cf9a937636f9fa16837e7ab4cddf78713
Michael Wolter <michael.wolter@atns.de>
2013-08-29 14:21:52 +0200

    view updates

--- src/de/capgeti/vereinlager/Main.java
+++ src/de/capgeti/vereinlager/Main.java
@@ -1,14 +1,11 @@
 package de.capgeti.vereinlager;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
-    public Main() {
-
-        JLa
-
-    }
-
     public static void main(String[] args) {
-        new Main();
+        new MainView().showWindow();
     }
 }
--- src/de/capgeti/vereinlager/model/Gruppe.java
+++ src/de/capgeti/vereinlager/model/Gruppe.java
@@ -5,4 +5,25 @@ package de.capgeti.vereinlager.model;
  * @since 28.08.13 15:51
  */
 public class Gruppe {
+    private String name;
+    private int aufLager;
+    private int inBenutzung;
+
+    public Gruppe(String name, int aufLager, int inBenutzung) {
+        this.name = name;
+        this.aufLager = aufLager;
+        this.inBenutzung = inBenutzung;
+    }
+
+    public int getAufLager() {
+        return aufLager;
+    }
+
+    public int getInBenutzung() {
+        return inBenutzung;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
